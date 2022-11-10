1e6ae803213deda0e0cb1cdbfeb6b62368d0962f
Praveen Bhawantha <1praveenbhawantha@gmail.com>
2022-11-10 07:54:03 +0530

    first express app

--- src/server.js
+++ src/server.js
@@ -8,4 +8,5 @@ app.use(express.json());
 
 app.use("/products", ProductRoute);
 
-app.listen(3333, () => console.log("Server is running on port 3333"));
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log("Server is running on port 3333"));
